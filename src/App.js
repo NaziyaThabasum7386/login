f18fd31a13ebb87fa8e5a75e3f0d1c99ee4fae0a
nimagalunaziya <nimagalunaziya@gmail.com>
2024-03-22 12:47:29 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+const haandleSubmit =(e) => {
+  e.preventDefault();
+  if(username === "user" && password === "password") {
+    setError("");
+    setIsSubmitted(true);
+
+  }else {
+    
+    setError("Invalid username or password");
+    setIsSubmitted(false);
+  }
+};
+
+  return (
+    <div> 
+      <h1>Login Page</h1>
+      {isSubmitted? (
+          <div>
+            <p>Welcome, {username}</p>
+          </div>
+         ) : (
+            <form onSubmit= {haandleSubmit}>
+              {error && <p> {error}</p>}
+              <div>
+                <label htmlFor="username">username:</label>
+                <input 
+                id = "username"
+                type= "text"
+                placeholder="username"
+                value= {username}
+                onChange={e => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">password:</label>
+                <input 
+                id = "password"
+                type= "text"
+                placeholder="password"
+                value= {password}
+                onChange={e => setPassword(e.target.value)}
+                />
+              </div>
+              <div>
+                <button type = "submit">Submit</button>
+              </div>
+            </form>
+          
+        )
+      }
+    </div>
+  )
+}
